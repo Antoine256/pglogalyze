3273bf836fd181c214f3d402c676df056831eb4e
Antoine Dupasquier <dupasquierantoine@gmail.com>
2026-03-26 16:08:04 +0100

    fixe empty lines

--- internal/postgresParsing/fileParsing.go
+++ internal/postgresParsing/fileParsing.go
@@ -13,8 +13,19 @@ func parseFile(file []byte) []string {
 	r, _ := regexp.Compile(`^\d\d\d\d-\d\d-\d\d \d\d:\d\d:\d\d\.\d\d\d \w+ \[\d+\] \w+: .*`)
 	r2, _ := regexp.Compile(`^\d\d\d\d-\d\d-\d\d \d\d:\d\d:\d\d\.\d\d\d \w+ \[\d+\] \w+@\w+ \w+: .*`)
 	i := 0
-	buffer := ""
+	//On va à la première ligne correspondante à un log :
+
+	for i < len(splitFile) && (r.MatchString(splitFile[i]) == false && r2.MatchString(splitFile[i]) == false) {
+		i++
+	}
+
+	buffer := splitFile[i]
 	bufferAP := false
+
+	if r2.MatchString(splitFile[i]) {
+		// c'est une ligne avec un user@database (il faut pouvoir les reconnaitre)
+		bufferAP = true
+	}
 	for i < len(splitFile) {
 		if i > 0 && (r.MatchString(splitFile[i]) || r2.MatchString(splitFile[i])) {
 			//on valide la ligne précédente si la ligne actuelle commence par le log préfix
